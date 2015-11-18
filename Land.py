606e1ce7cf82c1fbe019b404985e10be21860932
alstjq9292 <alstjq9292@naver.com>
2015-11-18 10:33:36 +0900

    Land.py 생성

---
+++ Land.py
@@ -0,0 +1,36 @@
+__author__ = 'Min'
+
+import game_framework
+
+import main_state
+from pico2d import *
+
+name = "land"
+
+class Land:
+    pass
+
+def enter():
+    pass
+
+
+def exit():
+    pass
+
+def update():
+    pass
+
+def draw():
+
+    clear_canvas()
+
+    update_canvas()
+
+def handle_events():
+    events = get_events()
+    pass
+
+def pause(): pass
+
+
+def resume(): pass
